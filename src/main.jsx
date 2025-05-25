93f69b6f8ba7b3c766a173d3625239563f232947
RencyBoreh <jeptanuirency313@gmail.com>
2025-05-25 23:21:33 +0300

    change

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from './App.jsx'
 import App2 from './App2.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
+   <HashRouter>
     <App2 />
     {/* <App /> */}
-  </BrowserRouter>,
+  </HashRouter>,
 )
